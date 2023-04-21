2e9f40e1e93a34e7ddcc02c2fbdbdba9fd6c0767
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-04-21 10:07:19 +0200

    added bigger floating terminal

--- after/plugin/nvterm.lua
+++ after/plugin/nvterm.lua
@@ -5,10 +5,10 @@ require("nvterm").setup({
     type_opts = {
       float = {
         relative = 'editor',
-        row = 0.3,
-        col = 0.25,
-        width = 0.5,
-        height = 0.4,
+        row = 0.05,
+        col = 0.05,
+        width = 0.9,
+        height = 0.8,
         border = "single",
       },
       horizontal = { location = "rightbelow", split_ratio = .3, },
